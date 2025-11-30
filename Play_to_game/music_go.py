29d98d8e2b8edda318f75e6de51b985fc41e8787
Ivan <levchenkoivan3007@gmail.com>
2025-11-30 11:55:17 +1000

    Update music_go.py

--- Play_to_game/music_go.py
+++ Play_to_game/music_go.py
@@ -1,6 +1,7 @@
 import os
-print("=== ПРОВЕРКА ФАЙЛОВ ===")
 for file in os.listdir('.'):
-    if 'wnm' in file.lower() or 'mp3' in file:
-        print(f"Найден файл: {file} (размер: {os.path.getsize(file)} байт)")
+    if 'wnm' in file.lower():
+        os.rename(file, 'WNM.mp3')
+        print(f"Файл переименован в WNM.mp3")
         input()
+        break
