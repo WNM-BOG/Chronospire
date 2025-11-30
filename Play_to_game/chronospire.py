1e7d9d4d30b48b0d7c06bd5e7e4cf614dd015fc6
Ivan <levchenkoivan3007@gmail.com>
2025-11-30 11:57:34 +1000

    Update chronospire.py

--- Play_to_game/chronospire.py
+++ Play_to_game/chronospire.py
@@ -2,10 +2,10 @@ import random
 import time
 import sys
 import os
-
 for file in os.listdir('.'):
     if 'wnm' in file.lower():
         os.rename(file, 'WNM.mp3')
+        print(f"Файл переименован в WNM.mp3")
         break
 
 player_name = ""
